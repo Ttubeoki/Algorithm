de8312df4b77c33ad1d312f3434aafb6616345fc
Ttubeoki <wndnjs19@gmail.com>
2022-11-14 21:31:01 +0900

    1. 알고리즘 업로드

---
+++ src/Codillity/Leader/Dominator.kt
@@ -0,0 +1,13 @@
+/**
+A[0] = 3    A[1] = 4    A[2] =  3
+A[3] = 2    A[4] = 3    A[5] = -1
+A[6] = 3    A[7] = 3
+ */
+
+fun main() {
+    println(solution(intArrayOf(3,4,3,2,3,-1,3,3)))
+}
+
+private fun solution(A: IntArray): Int {
+    return 1
+}
