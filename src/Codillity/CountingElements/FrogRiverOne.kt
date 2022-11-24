0f20ab0daf4d029b29b6942864754343414232c5
Ttubeoki <wndnjs19@gmail.com>
2022-11-24 23:56:16 +0900

    1. 알고리즘 업로드

--- src/Codillity/CountingElements/FrogRiverOne.kt
+++ src/Codillity/CountingElements/FrogRiverOne.kt
@@ -2,7 +2,6 @@ fun main() {
 
     println(solution(5,intArrayOf(1,3,1,4,2,3,5,4)))
     println(solution(5,intArrayOf(1,3,1,4,2,3,5,4)))
-    println(solution(5,intArrayOf(1,3,1,4,2,3,5,4)))
 }
 
 private fun solution(X: Int, A: IntArray): Int {
